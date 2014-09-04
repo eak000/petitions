47bab11b2f579e42ba9f4174e4d7987390b5b2ba
Emily Kelton <emilyk000@gmail.com>
2014-09-04 16:38:02 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+	$('form').submit (function(event) {
+		event.preventDefault();
+		//get search term values
+		var searchTags = $('#tags').val();
+		//show user search is happening
+		$('#search').prop("disabled", true);
+  	$('#submit').attr("disabled", true).val("searching....");
+
+  	// AJAX request to API
+  	var request = {
+  			isSignable: true,
+  			title: searchTags,
+  			body: searchTags,
+  			site: 'whitehouse'
+  	};
+
+  	var result = $.ajax({
+  		url: 'https://api.whitehouse.gov/v1/petitions.jsonp',
+  		data: request,
+  		dataType: "jsonp",
+  		type: "GET"
+  	})
+  	.done(function(result) {
+  		$.each(result.results, function(i, result){
+  			
+  			$('#resultsList').append('<li>'+ results.title + '<a href = "' + results.url + '"></a></li>');
+
+  		}); //end each
+
+  		$('#search').prop("disabled", false);
+  		$('#submit').attr("disabled", false).val("Search");
+  	})
+
+  
+	}); //end submit function
+
+
+}); //end ready function
